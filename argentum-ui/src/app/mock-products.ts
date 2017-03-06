0be2aeb803be5db0320dd40946ba5d941e23932e
Georg Guba <georg.guba@qaware.de>
2017-03-06 10:33:46 +0100

    product range button grid

---
+++ argentum-ui/src/app/mock-products.ts
@@ -0,0 +1,15 @@
+export const PRODUCTS = [
+  { id: 1, name: "Water" },
+  { id: 2, name: "Beer" },
+  { id: 3, name: "Long Drink" },
+  { id: 4, name: "Tap Water" },
+  { id: 5, name: "Cocktail" },
+  { id: 6, name: "Sprite" },
+  { id: 7, name: "Coke" },
+  { id: 8, name: "Pepsi" },
+  { id: 9, name: "Tea" },
+  { id: 10, name: "Chai Latte" },
+  { id: 11, name: "Coffee" },
+  { id: 12, name: "Red Bull" },
+  { id: 13, name: "Shot" }
+];
