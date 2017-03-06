247afebd8f954cb33b587a191a347b053deaf81c
Georg Guba <georg.guba@qaware.de>
2017-03-06 09:46:57 +0100

    basic angular setup with bootstrap and sass

--- argentum-ui/src/app/app.module.ts
+++ argentum-ui/src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
-import { AppComponent } from './app.component';
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
+import {AppComponent} from "./app.component";
+import {OrderComponent} from "./order/order.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrderComponent
   ],
   imports: [
     BrowserModule,
