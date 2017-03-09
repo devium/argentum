853ee5373bad1bab0f8b18b90c408f5dbbc6366d
Georg Guba <georg.guba@qaware.de>
2017-03-09 16:59:31 +0100

    item editor base & categories

--- argentum-ui/src/app/app-routing/app-routing.module.ts
+++ argentum-ui/src/app/app-routing/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { OrderViewComponent } from "../order-view/order-view.component";
+import { AdminViewComponent } from "../admin-view/admin-view.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/order', pathMatch: 'full' },
-  { path: 'order', component: OrderViewComponent }
+  { path: 'order', component: OrderViewComponent },
+  { path: 'admin', component: AdminViewComponent }
 ];
 @NgModule({
   imports: [
