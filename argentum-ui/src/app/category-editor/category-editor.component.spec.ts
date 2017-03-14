80aa7fac99859bbdf8f5b61dacb185ba7a75de35
Georg Guba <georg.guba@qaware.de>
2017-03-14 10:06:52 +0100

    category editor

---
+++ argentum-ui/src/app/category-editor/category-editor.component.spec.ts
@@ -0,0 +1,24 @@
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { CategoryEditorComponent } from "./category-editor.component";
+
+describe('CategoryEditorComponent', () => {
+  let component: CategoryEditorComponent;
+  let fixture: ComponentFixture<CategoryEditorComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [CategoryEditorComponent]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CategoryEditorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
