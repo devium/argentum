751097b8c27486337455d353c267b0467ff4ad0e
Georg Guba <georg.guba@qaware.de>
2017-03-15 15:08:10 +0100

    admin tabs using router

--- argentum-ui/src/app/app-routing/app-routing.module.ts
+++ argentum-ui/src/app/app-routing/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { OrderViewComponent } from "../order-view/order-view.component";
-import { AdminViewComponent } from "../admin-view/admin-view.component";
+import { ProductEditorComponent } from "../product-editor/product-editor.component";
+import { RangeEditorComponent } from "../range-editor/range-editor.component";
+import { CategoryEditorComponent } from "../category-editor/category-editor.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/order', pathMatch: 'full' },
   { path: 'order', component: OrderViewComponent },
-  { path: 'admin', component: AdminViewComponent }
+  { path: 'admin', redirectTo: '/admin/products', pathMatch: 'full' },
+  { path: 'admin/products', component: ProductEditorComponent },
+  { path: 'admin/categories', component: CategoryEditorComponent },
+  { path: 'admin/ranges', component: RangeEditorComponent }
 ];
 @NgModule({
   imports: [
