f035aefcf0d84d7135f800cfeb5c7c2288985cbf
Georg Guba <georg.guba@gmail.com>
2017-03-16 15:58:52 +0100

    guest import skeleton

---
+++ argentum-ui/src/app/admin/guest-import/guest-import.component.spec.ts
@@ -0,0 +1,24 @@
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { GuestImportComponent } from "./guest-import.component";
+
+describe('GuestImportComponent', () => {
+  let component: GuestImportComponent;
+  let fixture: ComponentFixture<GuestImportComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [GuestImportComponent]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(GuestImportComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ argentum-ui/src/app/admin/guest-import/guest-import.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: 'app-guest-import',
+  templateUrl: 'guest-import.component.html',
+  styleUrls: ['guest-import.component.scss']
+})
+export class GuestImportComponent implements OnInit {
+  codeCol = '';
+  nameCol = '';
+  mailCol = '';
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  import(file: File) {
+    let reader = new FileReader();
+    reader.onload = (event: ProgressEvent) => this.parse((<FileReader>event.target).result);
+    reader.readAsText(file);
+  }
+
+  parse(content: string) {
+
+  }
+
+}
