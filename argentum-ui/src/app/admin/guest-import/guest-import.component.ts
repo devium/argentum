a17fbf30a05180487a2796c57a81f6eaf60c1237
Georg Guba <georg.guba@gmail.com>
2017-03-20 13:38:02 +0100

    guest status field

--- argentum-ui/src/app/admin/guest-import/guest-import.component.ts
+++ argentum-ui/src/app/admin/guest-import/guest-import.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-guest-import',
@@ -9,6 +9,7 @@ export class GuestImportComponent implements OnInit {
   codeCol = '';
   nameCol = '';
   mailCol = '';
+  statusCol = '';
 
   constructor() {
   }
