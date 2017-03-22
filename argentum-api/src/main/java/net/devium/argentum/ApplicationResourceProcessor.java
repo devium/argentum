bebc63de3fd4e771d0eb95b97adefdb3d1feb484
Georg Guba <georg.guba@gmail.com>
2017-03-22 17:58:48 +0100

    fixed resource links

--- argentum-api/src/main/java/net/devium/argentum/ApplicationResourceProcessor.java
+++ argentum-api/src/main/java/net/devium/argentum/ApplicationResourceProcessor.java
@@ -1,5 +1,7 @@
 package net.devium.argentum;
 
+import net.devium.argentum.rest.OrderController;
+import net.devium.argentum.rest.ProductController;
 import net.devium.argentum.rest.ProductRangeController;
 import org.springframework.data.rest.webmvc.RepositoryLinksResource;
 import org.springframework.hateoas.ResourceProcessor;
@@ -11,8 +13,8 @@ public class ApplicationResourceProcessor implements ResourceProcessor<Repositor
     @Override
     public RepositoryLinksResource process(RepositoryLinksResource resource) {
         resource.add(ControllerLinkBuilder.linkTo(ProductRangeController.class).withRel("product_ranges"));
-        resource.add(ControllerLinkBuilder.linkTo(ProductRangeController.class).withRel("products"));
-        resource.add(ControllerLinkBuilder.linkTo(ProductRangeController.class).withRel("orders"));
+        resource.add(ControllerLinkBuilder.linkTo(ProductController.class).withRel("products"));
+        resource.add(ControllerLinkBuilder.linkTo(OrderController.class).withRel("orders"));
         return resource;
     }
 }
