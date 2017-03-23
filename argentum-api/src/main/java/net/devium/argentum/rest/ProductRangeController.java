67f8481ec86b272d099f236f7ac926b17be0bc49
Georg Guba <georg.guba@gmail.com>
2017-03-23 13:01:40 +0100

    ProductRangeController uses proper responses and error codes

--- argentum-api/src/main/java/net/devium/argentum/rest/ProductRangeController.java
+++ argentum-api/src/main/java/net/devium/argentum/rest/ProductRangeController.java
@@ -2,15 +2,11 @@ package net.devium.argentum.rest;
 
 import net.devium.argentum.jpa.ProductRangeEntity;
 import net.devium.argentum.jpa.ProductRangeRepository;
-import net.devium.argentum.rest.model.ProductRangeRequest;
-import net.devium.argentum.rest.model.ProductRangeResponseEager;
-import net.devium.argentum.rest.model.ProductRangeResponseMeta;
-import net.devium.argentum.rest.model.ProductResponseMeta;
+import net.devium.argentum.rest.model.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,7 +15,6 @@ import java.lang.invoke.MethodHandles;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
 
 @RestController
 @RequestMapping("/product_ranges")
@@ -33,22 +28,22 @@ public class ProductRangeController {
     }
 
     @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<List<ProductRangeResponseMeta>> getProductRanges() {
-        List<ProductRangeResponseMeta> response = StreamSupport.stream(productRangeRepository.findAll()
-                .spliterator(), false)
+    public ResponseEntity<?> getProductRanges() {
+        List<ProductRangeResponseMeta> response = productRangeRepository.findAll().stream()
                 .map(range -> new ProductRangeResponseMeta(range.getId(), range.getName()))
                 .collect(Collectors.toList());
 
-        return ResponseEntity.ok(response);
+        return Response.ok(response);
     }
 
     @RequestMapping(path = "/{rangeId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<ProductRangeResponseEager> getProductRange(@PathVariable Integer rangeId) {
+    public ResponseEntity<?> getProductRange(@PathVariable Integer rangeId) {
         ProductRangeEntity range = productRangeRepository.findOne(rangeId);
 
         if (range == null) {
-            LOGGER.info("Product range with ID {} not found.", rangeId);
-            throw new ResourceNotFoundException();
+            String message = String.format("Product range with ID %s not found.", rangeId);
+            LOGGER.info(message);
+            return Response.notFound(message);
         }
 
         List<ProductResponseMeta> products = range.getProducts().stream()
@@ -56,27 +51,27 @@ public class ProductRangeController {
                 .collect(Collectors.toList());
 
         ProductRangeResponseEager response = new ProductRangeResponseEager(range.getId(), range.getName(), products);
-        return ResponseEntity.ok(response);
+        return Response.ok(response);
     }
 
     @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
             produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity<ProductRangeResponseMeta> createProductRange(@RequestBody ProductRangeRequest range) {
+    public ResponseEntity<?> createProductRange(@RequestBody ProductRangeRequest range) {
         ProductRangeEntity newRange = new ProductRangeEntity(range.getName());
         newRange = productRangeRepository.save(newRange);
 
         ProductRangeResponseMeta response = new ProductRangeResponseMeta(newRange.getId(), newRange.getName());
-        return ResponseEntity.ok(response);
+        return Response.ok(response);
     }
 
     @RequestMapping(path = "/{rangeId}", method = RequestMethod.DELETE)
-    public ResponseEntity<Void> deleteProductRange(@PathVariable Integer rangeId) {
+    public ResponseEntity<?> deleteProductRange(@PathVariable Integer rangeId) {
         try {
             productRangeRepository.delete(rangeId);
         } catch (EmptyResultDataAccessException e) {
             String message = String.format("Product range %s not found.", rangeId);
             LOGGER.info(message, e);
-            throw new ResourceNotFoundException(message);
+            return Response.notFound(message);
         }
 
         return ResponseEntity.noContent().build();
@@ -87,7 +82,7 @@ public class ProductRangeController {
         if (rangeIds.isEmpty()) {
             String message = "No ranges to delete.";
             LOGGER.info(message);
-            return ResponseEntity.badRequest().body(message);
+            return Response.badRequest(message);
         }
 
         List<Integer> unknownRanges = rangeIds.stream()
@@ -97,7 +92,7 @@ public class ProductRangeController {
         if (!unknownRanges.isEmpty()) {
             String message = String.format("Product range(s) %s not found.", unknownRanges);
             LOGGER.info(message);
-            return ResponseEntity.badRequest().body(message);
+            return Response.notFound(message);
         }
 
         rangeIds.forEach(productRangeRepository::delete);
