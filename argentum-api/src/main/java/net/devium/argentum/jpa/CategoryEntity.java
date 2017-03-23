1d0cb6816d4c68000ee8504b9fb5dc51b5ab16bd
Georg Guba <georg.guba@gmail.com>
2017-03-23 21:57:18 +0100

    POST on /product_ranges performs a merge

--- argentum-api/src/main/java/net/devium/argentum/jpa/CategoryEntity.java
+++ argentum-api/src/main/java/net/devium/argentum/jpa/CategoryEntity.java
@@ -18,15 +18,15 @@ public class CategoryEntity {
     public CategoryEntity() {
     }
 
-    // PUT
-    public CategoryEntity(long id, String name, String color) {
-        this.id = id;
+    // Create
+    public CategoryEntity(String name, String color) {
         this.name = name;
         this.color = color;
     }
 
-    // POST
-    public CategoryEntity(String name, String color) {
+    // Update
+    public CategoryEntity(long id, String name, String color) {
+        this.id = id;
         this.name = name;
         this.color = color;
     }
