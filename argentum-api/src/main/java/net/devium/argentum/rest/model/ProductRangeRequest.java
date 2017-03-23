6449ec8290d766f4a52c6e828ceeb0795b0e2d0a
Georg Guba <georg.guba@gmail.com>
2017-03-23 14:14:25 +0100

    request objects constructors removed

--- argentum-api/src/main/java/net/devium/argentum/rest/model/ProductRangeRequest.java
+++ argentum-api/src/main/java/net/devium/argentum/rest/model/ProductRangeRequest.java
@@ -3,13 +3,6 @@ package net.devium.argentum.rest.model;
 public class ProductRangeRequest {
     private String name;
 
-    public ProductRangeRequest() {
-    }
-
-    public ProductRangeRequest(String name) {
-        this.name = name;
-    }
-
     public String getName() {
         return name;
     }
