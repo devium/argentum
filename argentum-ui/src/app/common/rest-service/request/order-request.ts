444b8dd254e23ddf1de5c5a5786da99968681e1a
Georg Guba <georg.guba@gmail.com>
2017-03-26 03:14:09 +0200

    support for custom products

--- argentum-ui/src/app/common/rest-service/request/order-request.ts
+++ argentum-ui/src/app/common/rest-service/request/order-request.ts
@@ -3,11 +3,18 @@ import { fromOrderItem, OrderItemRequest } from './order-item-request';
 export class OrderRequest {
   guestId: number;
   items: OrderItemRequest[];
+  customTotal: number;
 }
 
 export function fromOrder(order: Order): OrderRequest {
   return {
     guestId: order.guest.id,
-    items: Array.from(order.products.entries()).map(item => fromOrderItem(item))
+    items: Array.from(order.products.entries())
+      .filter(item => item[0].id != -1)
+      .map(item => fromOrderItem(item)),
+    customTotal: Array.from(order.products.entries())
+      .filter(item => item[0].id == -1)
+      .map(item => item[0].price * item[1])
+      .reduce((a, b) => a + b, 0)
   };
 }
