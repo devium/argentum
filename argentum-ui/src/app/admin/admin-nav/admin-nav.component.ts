4622360c192520b93b880c35cd075d3e92a42296
Georg Guba <georg.guba@gmail.com>
2017-09-03 20:40:40 +0200

    navbars & status editor reset fix & html reformatting

--- argentum-ui/src/app/admin/admin-nav/admin-nav.component.ts
+++ argentum-ui/src/app/admin/admin-nav/admin-nav.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['admin-nav.component.scss']
 })
 export class AdminNavComponent implements OnInit {
+  navbarCollapsed = true;
 
   constructor() {
   }
--- argentum-ui/src/app/common/navbar/navbar.component.ts
+++ argentum-ui/src/app/common/navbar/navbar.component.ts
@@ -7,6 +7,7 @@ import { RoleBasedComponent } from '../role-based/role-based.component';
   styleUrls: ['navbar.component.scss']
 })
 export class NavbarComponent extends RoleBasedComponent implements OnInit {
+  navbarCollapsed = true;
 
   constructor() {
     super();
