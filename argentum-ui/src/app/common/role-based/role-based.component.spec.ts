3669cc66a37b3740519fae93a85ba9b349334507
Georg Guba <georg.guba@gmail.com>
2018-09-13 16:48:01 +0200

    testing role-based component

--- argentum-ui/src/app/common/role-based/role-based.component.spec.ts
+++ argentum-ui/src/app/common/role-based/role-based.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RoleBasedComponent } from './role-based.component';
 
-xdescribe('RoleBasedComponent', () => {
+describe('RoleBasedComponent', () => {
   let component: RoleBasedComponent;
   let fixture: ComponentFixture<RoleBasedComponent>;
 
@@ -16,10 +16,29 @@ xdescribe('RoleBasedComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(RoleBasedComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should read roles from local storage', () => {
+    spyOn(localStorage, 'getItem').and.callFake((item: string) => {
+      return item === 'roles' ? 'ADMIN,SCAN,ORDER' : null;
+    });
+    fixture.detectChanges();
+    expect(component.roles).toEqual(['ADMIN', 'SCAN', 'ORDER']);
+  });
+
+  it('should properly check for any of the queried roles', () => {
+    spyOn(localStorage, 'getItem').and.callFake((item: string) => {
+      return item === 'roles' ? 'ADMIN,SCAN,ORDER' : null;
+    });
+    fixture.detectChanges();
+    expect(component.hasRole(['ADMIN'])).toBeTruthy();
+    expect(component.hasRole(['SCAN'])).toBeTruthy();
+    expect(component.hasRole(['CHECKIN', 'SCAN'])).toBeTruthy();
+    expect(component.hasRole(['CHECKIN'])).toBeFalsy();
+    expect(component.hasRole(['CHECKIN', 'ALL_RANGES'])).toBeFalsy();
   });
 });
