f42d9a6f29fc2b1903fc88f5e292221c7ca33132
Georg Guba <georg.guba@gmail.com>
2018-10-30 12:49:37 +0100

    django guest model

--- argentum-django/api/models.py
+++ argentum-django/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Guest(models.Model):
+    code = models.CharField(max_length=32)
+    name = models.CharField(max_length=64)
+    mail = models.CharField(max_length=64)
+    status = models.CharField(max_length=32)
+    checked_in = models.DateTimeField()
+    card = models.CharField(max_length=32)
+    balance = models.DecimalField(max_digits=9, decimal_places=2)
+    bonus = models.DecimalField(max_digits=9, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.name} ({self.code})'
